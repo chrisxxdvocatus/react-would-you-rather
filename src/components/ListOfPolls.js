4ce053cf47f7226f81699efc9aa0804852b94e42
EK XX <chrisesq@protonmail.com>
2020-07-11 00:42:53 -0400

    Polls.js state

--- src/components/ListOfPolls.js
+++ src/components/ListOfPolls.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
+import Polls from './Polls'
 
 class ListOfPolls extends Component {
     
     render(){
         console.log('ListOfPolls: ',this.props)
+
         return(
             <div>
-               ListOfPolls
+                <h3>ListOfPolls</h3>
+               <div>{this.props.questionsIds.map((id)=>(
+                    <li key ={id}>
+                        <Polls id={id} />
+                    </li>
+                    ))}
+               </div>
             </div>
         )
     }
 }
 
+function mapStateToProps ({ questions }) {
+    
+    return {
+      questionsIds: Object.keys(questions)
+        .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
+    }
+  }
 
-
-export default connect()(ListOfPolls)
+export default connect(mapStateToProps)(ListOfPolls)
---
+++ src/utils/helper.js
@@ -0,0 +1,9 @@
+import {
+    formatQuestion,
+} from './_DATA.js'
+
+
+export function formatQuestion ({ optionOneText, optionTwoText, author }) {
+    return formatQuestion ({ optionOneText, optionTwoText, author }) 
+}
+
